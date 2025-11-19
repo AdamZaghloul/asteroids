745ced896032e35c581fa19566553122396461e5
AdamZaghloul <adam.zaghloul95@gmail.com>
2025-11-18 21:13:54 -0500

    Updated with logger.

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from asteroidfield import *
 from shot import *
 
 def main():
-    print("Starting asteroids!")
+    print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
